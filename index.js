3a9274a313683b0fbcb7fa7477ae3b8813de9578
LifeAsDev <77514129+LifeAsDev@users.noreply.github.com>
2023-09-29 20:57:19 +0000

    First Commit

--- index.js
+++ index.js
@@ -7,14 +7,22 @@ var app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const multer = require('multer')
+const upload = multer({ dest: './public/data/uploads/' })
+
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res) => {
+  console.log(req.file, req.body)
+
+  res.json({ name: req.file.originalname, type: req.file.mimetype, size: req.file.size })
+})
 
 
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('Your app is listening on port ' + port)
 });
